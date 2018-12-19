da7bc41ffc05c785750f845a6b37a5b15def408a
Mathew Byrne <mathew.byrne@99designs.com>
2018-12-19 12:02:00 +1100

    Character explorer

--- client/index.jsx
+++ client/index.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
+import { ApolloProvider, Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App />, document.getElementById('app'))
@@ -14,5 +15,51 @@ const client = new ApolloClient({
 const App = () => (
   <ApolloProvider client={client}>
     <h1>Rick &amp; Morty Explorer</h1>
+    <CharacterExplorer />
   </ApolloProvider>
+)
+
+const CharacterExplorer = () => {
+  const [page, setPage] = useState(1)
+  return (
+    <Query
+      query={gql`
+        query Characters($page: Int!) {
+          characters(page: $page) {
+            id
+            name
+            image
+          }
+        }
+      `}
+      variables={{ page }}
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>
+        if (error) return <p>Error</p>
+
+        return (
+          <div className="character-explorer">
+            <div className="character-explorer__controls">
+              {page > 1 && <button onClick={() => setPage(page - 1)}>Prev</button>}
+              Page {page}
+              <button onClick={() => setPage(page + 1)}>Next</button>
+            </div>
+            <CharacterList characters={data.characters} />
+          </div>
+        )
+      }}
+    </Query>
+  )
+}
+
+const CharacterList = ({ characters }) => (
+  <div className="character-list">
+    {characters.map(character => (
+      <div className="character-list__item" key={`character-${character.id}`}>
+        <img src={character.image} alt={character.name} />
+        <div className="character-list__name">{character.name}</div>
+      </div>
+    ))}
+  </div>
 )
