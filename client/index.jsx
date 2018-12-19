26c3e8f0719026356067246ba807e398f19cb760
Mathew Byrne <mathew.byrne@99designs.com>
2018-12-19 14:55:06 +1100

    Pagination component

--- client/index.jsx
+++ client/index.jsx
@@ -23,40 +23,54 @@ const CharacterExplorer = () => {
   const [page, setPage] = useState(1)
   const [character, setCharacter] = useState(null)
   return (
-    <Query
-      query={gql`
-        query Characters($page: Int!) {
-          characters(page: $page) {
-            id
-            name
-            image
+    <div className="character-explorer">
+      <div className="character-explorer__controls">
+        <PaginationControls page={page} setPage={setPage} />
+      </div>
+      <Query
+        query={gql`
+          query Characters($page: Int!) {
+            characters(page: $page) {
+              id
+              name
+              image
+            }
           }
-        }
-      `}
-      variables={{ page }}
-    >
-      {({ loading, error, data }) => {
-        if (loading) return <div className="loading">Loading...</div>
-        if (error) return <div className="error">Error</div>
+        `}
+        variables={{ page }}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <div className="loading">Loading...</div>
+          if (error) return <div className="error">Error</div>
 
-        return (
-          <div className="character-explorer">
-            <div className="character-explorer__controls">
-              {page > 1 && <button onClick={() => setPage(page - 1)}>Prev</button>}
-              Page {page}
-              <button onClick={() => setPage(page + 1)}>Next</button>
+          return (
+            <div>
+              <CharacterList
+                characters={data.characters}
+                onClick={character => setCharacter(character.id)}
+              />
+              {character && <CharacterModal id={character} onClose={() => setCharacter(null)} />}
             </div>
-            <CharacterList
-              characters={data.characters}
-              onClick={character => setCharacter(character.id)}
-            />
-            {character && <CharacterModal id={character} onClose={() => setCharacter(null)} />}
-          </div>
-        )
-      }}
-    </Query>
+          )
+        }}
+      </Query>
+    </div>
   )
 }
+
+const PaginationControls = ({ page, setPage }) => (
+  <div className="pagination">
+    <div className="pagination__prev">
+      {page > 1 && <button onClick={() => setPage(page - 1)}>Prev</button>}
+    </div>
+
+    <div className="pagination__page">Page {page}</div>
+
+    <div className="pagination__next">
+      <button onClick={() => setPage(page + 1)}>Next</button>
+    </div>
+  </div>
+)
 
 const CharacterList = ({ characters, onClick }) => (
   <div className="character-list">
@@ -106,7 +120,7 @@ const CharacterModal = ({ id, onClose }) => (
                   <dt>Species</dt>
                   <dd>{character.species}</dd>
                 </dl>
-                {character.isLiked ? '💜' : <FavouriteButton id={id} />}
+                <FavouriteButton id={id} />
               </div>
             </div>
           )
